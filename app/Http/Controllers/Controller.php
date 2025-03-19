6346f2520d8e4f291fa3c43393a46f6a594c74ba
yassine snaiki <yassnaiki@gmail.com>
2025-03-19 10:44:45 +0000

    configuring swagger

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -2,6 +2,21 @@
 
 namespace App\Http\Controllers;
 
+/**
+ * @OA\Info(
+ *     title="crud-example",
+ *     version="1.0.0",
+ *     description="crud example api"
+ * )
+ * @OA\Server(
+ *     url="http://crud-example.test/api",
+ *     description="Local API Server"
+ * )
+ * @OA\Tag(
+ *     name="Posts",
+ *     description="Endpoints for managing posts (CRUD)"
+ * )
+ */
 abstract class Controller
 {
     //
